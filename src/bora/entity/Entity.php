3c779041279d73270dbd448d7043a381d8f61ffd
bora-mp <48325827+bora-mp@users.noreply.github.com>
2019-03-17 14:39:30 +0300

    Zararsız 4 mob eklendi

--- src/bora/entity/Entity.php
+++ src/bora/entity/Entity.php
@@ -1,22 +1,23 @@
 <?php
 
 /*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.bora.net/
- *
- *
+*
+*    ____   ____  _____                 __  __ _____
+*   |  _ \ / __ \|  __ \     /\        |  \/  |  __ \
+*   | |_) | |  | | |__) |   /  \ ______| \  / | |__) |
+*   |  _ <| |  | |  _  /   / /\ \______| |\/| |  ___/
+*   | |_) | |__| | | \ \  / ____ \     | |  | | |
+*   |____/ \____/|_|  \_\/_/    \_\    |_|  |_|_|
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* @author Bora Team
+* @link http://bora.kodmadeni.com/
+*
+*
 */
 
 declare(strict_types=1);
